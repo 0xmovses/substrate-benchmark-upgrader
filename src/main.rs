35f752f6c9c2008e10b02a68b1f5f7472a0f7d81
0xmovses <r.v.melkonian@gmail.com>
2023-11-07 18:36:23 +0000

    parse fn params

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,33 @@
-use syn::{visit_mut::VisitMut, parse2, Item, punctuated::Punctuated, parse::{ParseStream, Parse, Result}, token::Semi, ItemMacro, ItemMod, Block, Stmt, Attribute, Ident, File, Token, ItemFn, braced, parse_quote, Visibility, Signature, ReturnType, Generics};
-use quote::{quote, ToTokens, format_ident};
-use proc_macro2::{Span, TokenStream, TokenTree};
-use std::{fs, iter::Peekable};
+use proc_macro2::{Delimiter, Span, TokenStream, TokenTree};
+use quote::{format_ident, quote, ToTokens};
 use regex;
+use std::{fs, iter::Peekable, result::Result};
 use syn::parse::Parser;
+use syn::{
+    braced,
+    parse::{Parse, ParseStream, Result as ParseResult},
+    parse2, parse_quote,
+    punctuated::Punctuated,
+    token::Semi,
+    visit_mut::VisitMut,
+    Attribute, Block, Expr, File, Generics, Ident, Item, ItemFn, ItemMacro, ItemMod, ReturnType,
+    Signature, Stmt, Token, Visibility,
+};
 
+enum RangeEndKind {
+    Number(u8),
+    Expression(Expr), // check, the DSL might not be valid syn::Expr ?
+}
+
+struct BenchmarkParameter {
+    name: Ident,
+    range_start: u8,
+    range_end: RangeEndKind,
+}
 struct Items(Vec<Item>);
 
 impl Parse for Items {
-    fn parse(input: ParseStream) -> Result<Self> {
+    fn parse(input: ParseStream) -> ParseResult<Self> {
         let mut items = Vec::new();
         while !input.is_empty() {
             items.push(input.parse()?);
@@ -23,7 +42,7 @@ struct BenchmarkFunction {
 }
 
 impl Parse for BenchmarkFunction {
-    fn parse(input: ParseStream) -> Result<Self> {
+    fn parse(input: ParseStream) -> ParseResult<Self> {
         let name: Ident = input.parse()?;
         let block = if input.peek(syn::token::Brace) {
             let content;
@@ -48,11 +67,18 @@ impl VisitMut for RefactorBenchmark {
             match item {
                 Item::Macro(i_macro) if i_macro.mac.path.is_ident("benchmarks") => {
                     let content = i_macro.mac.tokens.clone();
-                    let parsed_functions= parse_benchmark_functions(content);
+                    let parsed_functions = parse_benchmark_functions(content.clone());
 
-                    // Collet the transformed benchmark functions
+                    let parsed_params = parse_parameters(content).unwrap();
+                    for param in &parsed_params {
+                        println!("Param name: {}", param.name);
+                        println!("Param range start: {}", param.range_start);
+                    }
+
+                    // Collect the transformed benchmark functions
                     let mut transformed_functions = Vec::new();
                     for function in parsed_functions.into_iter() {
+                        let _params = parse_parameters(function.block.clone());
                         // Create a new Rust function item with the #[benchmark] attribute.
                         let attrs: Vec<Attribute> = vec![parse_quote!(#[benchmark])];
                         // Construct a raw block from the captured TokenStream
@@ -89,27 +115,26 @@ impl VisitMut for RefactorBenchmark {
                     };
 
                     new_items.push(Item::Verbatim(new_mod_tokens));
-                },
+                }
                 _ => new_items.push(item),
             }
         }
 
         // Create a new mod with the transformed benchmark functions.
-            let new_mod_tokens = quote! {
-                #[instance_benchmarks]
-                mod benchmarks {
-                    #(#new_items)*
-                }
-            };
-            // Push the new module to the list of items.
-            let new_mod = Item::Verbatim(new_mod_tokens);
-            other_items.push(new_mod);
+        let new_mod_tokens = quote! {
+            #[instance_benchmarks]
+            mod benchmarks {
+                #(#new_items)*
+            }
+        };
+        // Push the new module to the list of items.
+        let new_mod = Item::Verbatim(new_mod_tokens);
+        other_items.push(new_mod);
 
         // Update the items in the file with the new items.
         file.items = new_items;
     }
 }
-
 
 fn parse_benchmark_functions(input: TokenStream) -> Vec<BenchmarkFunction> {
     let mut functions = Vec::new();
@@ -133,6 +158,102 @@ fn parse_benchmark_functions(input: TokenStream) -> Vec<BenchmarkFunction> {
     functions
 }
 
+fn parse_parameters(input: TokenStream) -> Result<Vec<BenchmarkParameter>, String> {
+    let mut iter = input.into_iter();
+    let mut params = Vec::new();
+
+    while let Some(token) = iter.next() {
+        match token {
+            TokenTree::Ident(ident) if ident == "let" => {
+                if let Some(TokenTree::Ident(name)) = iter.next() {
+                    // Look for the 'in' keyword
+                    match iter.next() {
+                        Some(TokenTree::Ident(ident)) if ident == "in" => {
+                            // Parse range start...
+                            let range_start_token = iter.next().ok_or("Expected a range start")?;
+                            let range_start = token_tree_to_u8(range_start_token)?;
+
+                            // Parse '..' and range end...
+                            iter.next().ok_or("Expected '..' after range start")?;
+                            iter.next().ok_or("Expected '..' after range start")?;
+                            let range_end = parse_range_end(&mut iter)?;
+
+                            // Now skip all tokens until we find the '=>' operator
+                            while let Some(token) = iter.next() {
+                                match token {
+                                    TokenTree::Punct(ref punct) if punct.as_char() == '>' => {
+                                        // Found '=>', now look for ';'
+                                        while let Some(token) = iter.next() {
+                                            match token {
+                                                TokenTree::Punct(ref punct) if punct.as_char() == ';' => {
+                                                    // Found ';', terminate the parameter declaration
+                                                    break;
+                                                },
+                                                _ => continue, // Skip other tokens
+                                            }
+                                        }
+                                        break; // Break from the outer loop once ';' is found
+                                    },
+                                    _ => continue, // Skip other tokens before '=>'
+                                }
+                            }
+
+                            // Push the parsed parameter
+                            params.push(BenchmarkParameter {
+                                name,
+                                range_start,
+                                range_end,
+                            });
+                        },
+                        Some(_) | None => return Err("Expected 'in' keyword".to_string()),
+                    }
+                } else {
+                    return Err("Expected parameter name after 'let'".to_string());
+                }
+            },
+            _ => continue, // Ignore other tokens
+        }
+    }
+
+    for param in &params {
+        println!("Param name: {}", param.name);
+        println!("Param range start: {}", param.range_start);
+    }
+
+    Ok(params)
+}
+
+
+// A helper function to try to parse a TokenTree as a u8
+fn token_tree_to_u8(token: TokenTree) -> Result<u8, &'static str> {
+    if let TokenTree::Literal(lit) = token {
+        lit.to_string()
+            .parse::<u8>()
+            .map_err(|_| "Unable to parse as u8")
+    } else {
+        Err("Expected a number")
+    }
+}
+
+// A helper function to parse an expression from a group or an ident
+fn parse_range_end(
+    iter: &mut impl Iterator<Item = TokenTree>,
+) -> Result<RangeEndKind, &'static str> {
+    let token = iter.next().ok_or("Expected a token for range end")?;
+    match token {
+        TokenTree::Literal(lit) => {
+            token_tree_to_u8(TokenTree::Literal(lit)).map(RangeEndKind::Number)
+        }
+        TokenTree::Group(group) if group.delimiter() == Delimiter::Brace => parse2(group.stream())
+            .map(RangeEndKind::Expression)
+            .map_err(|_| "Unable to parse expression for range end"),
+        TokenTree::Ident(ident) => syn::parse_str(&ident.to_string())
+            .map(RangeEndKind::Expression)
+            .map_err(|_| "Unable to parse expression for range end"),
+        _ => Err("Expected a number or an expression for range end"),
+    }
+}
+
 fn main() {
     let mut benchmark = RefactorBenchmark;
     let file_contents = fs::read_to_string("src/fixtures/benchmark_v1.rs")
@@ -140,8 +261,7 @@ fn main() {
     let mut file_ast: File = syn::parse_str(&file_contents).expect("Failed to parse file");
     benchmark.visit_file_mut(&mut file_ast);
     let tokens = quote! { #file_ast };
-    println!("File AST is: {}", tokens.to_string());
-
+    //println!("File AST is: {}", tokens.to_string());
 }
 
 #[cfg(test)]
@@ -163,7 +283,8 @@ mod tests {
 
         let tokens = quote! { #file_ast };
         let output = tokens.to_string();
-        let expected_output = "#[instance_benchmarks] mod benchmarks { add_registrar { } }".replace(" ", "");
+        let expected_output =
+            "#[instance_benchmarks] mod benchmarks { add_registrar { } }".replace(" ", "");
         let sanitized_output = output.replace(" ", "");
 
         assert_eq!(
@@ -175,4 +296,3 @@ mod tests {
         println!("File AST is: {}", output);
     }
 }
-
